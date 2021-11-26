ace399e1e8280ac4e2e509c0f255d54ff4ee571f
Marcin <marcin.mroz011@gmail.com>
2021-11-26 18:23:37 +0000

    categories init

---
+++ src/components/Categories.vue
@@ -0,0 +1,148 @@
+<template>
+  <div class="maincategories">
+    <div class="categories-wrapper">
+      <h3 class="categories-header">Kategorie Główne</h3>
+      <div class="categories-container">
+        <div class="categories-item">
+          <a href="">
+            <span>Motoryzacja</span>
+            <img src="@/assets/categories/motoryzacja.png" alt="motoryzacja" />
+          </a>
+        </div>
+        <div class="categories-item">
+          <a href="">
+            <span>Nieruchomości</span>
+            <img src="@/assets/categories/motoryzacja.png" alt="motoryzacja" />
+          </a>
+        </div>
+        <div class="categories-item">
+          <a href="">
+            <span>Praca</span>
+            <img src="@/assets/categories/motoryzacja.png" alt="motoryzacja" />
+          </a>
+        </div>
+        <div class="categories-item">
+          <a href="">
+            <span>Dom i Ogród</span>
+            <img src="@/assets/categories/motoryzacja.png" alt="motoryzacja" />
+          </a>
+        </div>
+        <div class="categories-item">
+          <a href="">
+            <span>Elektronika</span>
+            <img src="@/assets/categories/motoryzacja.png" alt="motoryzacja" />
+          </a>
+        </div>
+        <div class="categories-item">
+          <a href="">
+            <span>Moda</span>
+            <img src="@/assets/categories/motoryzacja.png" alt="motoryzacja" />
+          </a>
+        </div>
+        <div class="categories-item">
+          <a href="">
+            <span>Rolnictwo</span>
+            <img src="@/assets/categories/motoryzacja.png" alt="motoryzacja" />
+          </a>
+        </div>
+        <div class="categories-item">
+          <a href="">
+            <span>Zwierzęta</span>
+            <img src="@/assets/categories/motoryzacja.png" alt="motoryzacja" />
+          </a>
+        </div>
+        <div class="categories-item">
+          <a href="">
+            <span>Dla Dzieci</span>
+            <img src="@/assets/categories/motoryzacja.png" alt="motoryzacja" />
+          </a>
+        </div>
+        <div class="categories-item">
+          <a href="">
+            <span>Sport i Hobby</span>
+            <img src="@/assets/categories/motoryzacja.png" alt="motoryzacja" />
+          </a>
+        </div>
+        <div class="categories-item">
+          <a href="">
+            <span>Muzyka i Edukacja</span>
+            <img src="@/assets/categories/motoryzacja.png" alt="motoryzacja" />
+          </a>
+        </div>
+        <div class="categories-item">
+          <a href="">
+            <span>Usługi i Firmy</span>
+            <img src="@/assets/categories/motoryzacja.png" alt="motoryzacja" />
+          </a>
+        </div>
+        <div class="categories-item">
+          <a href="">
+            <span>Noclegi</span>
+            <img src="@/assets/categories/motoryzacja.png" alt="motoryzacja" />
+          </a>
+        </div>
+        <div class="categories-item">
+          <a href="">
+            <span>Oddam za darmo</span>
+            <img src="@/assets/categories/motoryzacja.png" alt="motoryzacja" />
+          </a>
+        </div>
+        <div class="categories-item">
+          <a href="">
+            <span>Zamienię</span>
+            <img src="@/assets/categories/motoryzacja.png" alt="motoryzacja" />
+          </a>
+        </div>
+        <div class="categories-item">
+          <a href="">
+            <span>Pracuj jako dostawca</span>
+            <img src="@/assets/categories/motoryzacja.png" alt="motoryzacja" />
+          </a>
+        </div>
+        <div class="categories-item">
+          <a href="">
+            <span>Domek w górach</span>
+            <img src="@/assets/categories/motoryzacja.png" alt="motoryzacja" />
+          </a>
+        </div>
+        <div class="categories-item">
+          <a href="">
+            <span>Części z przesyłką za 1zł</span>
+            <img src="@/assets/categories/motoryzacja.png" alt="motoryzacja" />
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Categories",
+  props: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.maincategories {
+  background: white;
+  position: absolute;
+  margin-top: 220px;
+  left: 0;
+  right: 0;
+  padding-top: 56px;
+  padding-bottom: 61px;
+
+  .categories-wrapper {
+    width: 68vw;
+    padding: 0 24px;
+    margin: auto;
+
+    .categories-header {
+      font-size: 32px;
+      font-weight: bold;
+      margin: 0;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,19 @@
 <template>
-  <SearchBar></SearchBar>
+  <body>
+    <SearchBar></SearchBar>
+    <Categories></Categories>
+  </body>
 </template>
 <script>
 // @ is an alias to /src
 import SearchBar from "@/components/SearchBar.vue";
+import Categories from "@/components/Categories.vue";
 
 export default {
   name: "Home",
   components: {
     SearchBar,
+    Categories,
   },
 };
 </script>
